9a4f8231c66cd4b29b2d25ad45bb9a29b1516d2b
Joel Dalley <joeldalley@gmail.com>
2014-02-26 18:23:30 -0700

    re-order subs so _ is defined when used

--- Parser/Lexer/Std.pm
+++ Parser/Lexer/Std.pm
@@ -10,6 +10,19 @@ use JBD::Core::Exporter ':omni';
 
 no strict 'refs';
 
+# Helper sub for pattern-matchers.
+# @param string $text Arbitrary/Unstructured text.
+# @param regex $regex Pattern to match.
+# @return array Array of (remaining text, matched text).
+sub _($$) {
+    my ($text, $regex) = @_;
+    $text =~ $regex;
+    return ($text) unless defined $1 && length $1;
+    my $diff = length($text) - length $1;
+    $text = substr $text, length($1), $diff;
+    ($text, $1);
+}
+
 # Map of { package symbol => pattern-matcher sub }.
 my %symbol_definitions = (
     Dot   => sub { _ shift, qr{^(\.+)}o },
@@ -25,20 +38,5 @@ my %symbol_definitions = (
 while (my ($sym, $sub) = each %symbol_definitions) {
     *{__PACKAGE__ . "::$sym"} = sub { bless $sub, $sym };
 }
-
-
-# Helper sub for pattern-matchers.
-# @param string $text Arbitrary/Unstructured text.
-# @param regex $regex Pattern to match.
-# @return array Array of (remaining text, matched text).
-sub _ {
-    my ($text, $regex) = @_;
-    $text =~ $regex;
-    return ($text) unless defined $1 && length $1;
-    my $diff = length($text) - length $1;
-    $text = substr $text, length($1), $diff;
-    ($text, $1);
-}
-
 
 1;
