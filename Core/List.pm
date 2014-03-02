cd8cc81d3ef582255fef403f924113e0ef87e7cb
Joel Dalley <joeldalley@gmail.com>
2014-03-02 11:26:55 -0700

    add pairsof

--- Core/List.pm
+++ Core/List.pm
@@ -10,11 +10,23 @@ use JBD::Core::Exporter ':omni';
 use Carp 'croak';
 
 
-#///////////////////////////////////////////////////////////////
-#/ Functional Interface ////////////////////////////////////////
+# @param array an array
+# @return array a flat list
+sub flatmap(@) { 
+    map {
+        if    (ref $_ eq 'ARRAY')  { flatmap(@$_) }
+        elsif (ref $_ eq 'HASH')   { flatmap(%$_) }
+        elsif (ref $_ eq 'SCALAR') { $$_ }
+        else                       { $_ }
+    } @_
+}
 
-# @param array   an array
-# @return array    a shuffled array
+# @param array An array.
+# @return array An array with unique elements.
+sub uniq(@) { keys %{{map {$_ => undef} @_}} }
+
+# @param array An array.
+# @return array A shuffled array.
 sub shuffle(@) {
     my ($j, @l) = (0, @_);
     my $i = @l or return ();
@@ -22,35 +34,18 @@ sub shuffle(@) {
     @l;
 }
 
-# @param array    an array
-# @return array    a flat list
-sub flatmap(@) { _flatmap_recursive(@_) }
-
-# @param array    an array
-# @return array    an array with unique elements
-sub uniq(@) { keys %{{map {$_ => undef} @_}} }
-
-# @param array    an array
-# @return array    zip-ordered array (1st half, 2nd half)
+# @param array An even-sized array.
+# @return array A zip-ordered array (1st half, 2nd half).
 sub zip(@) { 
     croak 'Odd number of elements in zip' if @_ % 2;
     @_[map {$_, $_ + @_/2} 0 .. (@_/2 - 1)];
 }
 
-
-#///////////////////////////////////////////////////////////////
-#/ Used Internally /////////////////////////////////////////////
-
-# Uses recursion to completely flatten the given array.
-# @param array    array of arbitrary size, depth and structure
-# @return array    a flat list
-sub _flatmap_recursive {
-    map {
-        if    (ref $_ eq 'ARRAY')  { _flatmap_recursive(@$_) }
-        elsif (ref $_ eq 'HASH')   { _flatmap_recursive(%$_) }
-        elsif (ref $_ eq 'SCALAR') { $$_ }
-        else                       { $_ }
-    } @_
+# @param array An even-sized array.
+# @return coderef An iterator of pairs from the array.
+sub pairsof(@) {
+    croak 'Odd number of elements in pairsof' if @_ % 2;
+    my @L = @_; sub {@L ? [shift @L, shift @L] : undef};
 }
 
 1;
