06ccf49ecc3d59a375f358828fd8f61b5b00d19b
Joel Dalley <joeldalley@gmail.com>
2014-07-08 18:07:43 -0400

    update

--- lib/JBD/Javascript.pm
+++ lib/JBD/Javascript.pm
@@ -10,20 +10,12 @@ use JBD::Core::Exporter ':omni';
 
 use JBD::Parser::DSL;
 use JBD::Javascript::Lexers;
-use JBD::Javascript::Grammar;
-use JBD::Javascript::Transformers 'remove_novalue';
 
 # @param string $parser A JBD::Parser sub name.
 # @param scalar/ref $text Javascript text.
 # @return arrayref Array of JBD::Parser::Tokens.
 sub std_parse(@) {
     my ($parser, $text) = @_;
-
-    init;
-    my $st = parser_state tokens $text, [];
-
-    no strict 'refs';
-    remove_novalue &$parser->($st) or die $st->error_string;
 }
 
 1;
